d25edcd764fc7bd4f67ec20c38746f0007d8d348
Administrator <296642027@qq.com>
2016-06-13 18:56:56 +0800

    潘亚威撒比

---
+++ app/src/main/java/com/chenchengwei/myday30_shipei/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chenchengwei.myday30_shipei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //潘亚威撒比
+    }
+}
